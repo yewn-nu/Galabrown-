3fc0a1dbf7f6b4b858bd85710ca79c40b29e8a38
yewn-nu <eriksoto771@gmail.com>
2026-04-26 17:04:51 -0500

    Add files via upload

---
+++ proyectonavemarron-galaga/escenas/nubes.gd
@@ -0,0 +1,41 @@
+extends Node2D
+
+
+var velocidad = 150
+
+
+var nubes = []
+
+var texturas = [
+		preload("res://sprites/nube-chica (1).png"),
+		preload("res://sprites/nube-mediana (1).png"),
+		preload("res://sprites/nube-grande (1).png")
+	]
+
+func _ready():
+	
+	for i in range(8):
+		crear_nube()
+
+func crear_nube():
+	var nube = Sprite2D.new()
+	
+	nube.texture = texturas[randi() % texturas.size()]
+
+	nube.position.x = randf_range(0, 1280)
+	nube.position.y = randf_range(0, 720)
+	
+	
+	add_child(nube) # crear en si el objeto
+	nubes.append(nube)  #para que este se mueva
+	
+
+func _process(delta):
+	
+	for nube in nubes:
+		nube.position.y += velocidad * delta
+		
+		
+		if nube.position.y > 820:# cuando la nube se caiga la sube de nuevo y se repite el ciclosjdkd
+			nube.position.y = -100 
+			nube.position.x = randf_range(0, 1280)

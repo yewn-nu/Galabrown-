d49a93d76cb47f88e90b09d5ceeaf662bef3ac17
yewn-nu <eriksoto771@gmail.com>
2026-05-01 19:13:17 -0500

    Delete galabrown-main directory

--- galabrown-main/escenas/pricipal.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-var EnemigoEscena = preload("res://enemigo.tscn")
-
-func _ready():
-	var enemigo = EnemigoEscena.instantiate()
-	enemigo.position = Vector2(500, 100) # posición visible
-	add_child(enemigo)

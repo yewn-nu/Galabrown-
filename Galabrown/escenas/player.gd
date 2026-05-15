f62f1bb1d3388cbe3bf609bdf66e40761986930a
yewn-nu <eriksoto771@gmail.com>
2026-05-15 18:54:34 -0500

    HAY QUE SER SALADO

--- Galabrown/escenas/player.gd
+++ Galabrown/escenas/player.gd
@@ -8,35 +8,47 @@ var vida     = vida_max
 
 @onready var barra = $BarraVida
 
+
 func _ready():
 	barra.max_value = vida_max
-	barra.value     = vida_max
+	barra.value = vida_max
+
 
 func _process(delta):
 	var movimientoInput = Vector2()
 
 	if Input.is_action_pressed("ui_left"):
 		movimientoInput.x -= 1
+
 	if Input.is_action_pressed("ui_right"):
 		movimientoInput.x += 1
+
 	if Input.is_action_pressed("ui_up"):
 		movimientoInput.y -= 1
+
 	if Input.is_action_pressed("ui_down"):
 		movimientoInput.y += 1
 
 	movimientoInput = velocidad * movimientoInput.normalized()
 	position += movimientoInput * delta
 
+
 	if Input.is_action_just_pressed("ui_accept"):
 		var bala = BalaEscena.instantiate()
 		bala.global_position = get_node("SpawnBala").global_position
 		get_parent().add_child(bala)
 
+
 func recibir_danio(cantidad = 1):
 	vida -= cantidad
 	barra.value = vida
+
 	if vida <= 0:
 		_morir()
 
+
 func _morir():
-	get_tree().change_scene_to_file("res://pantallagameover.tscn")
+	get_tree().call_deferred(
+		"change_scene_to_file",
+		"res://pantallagameover.tscn"
+	)
